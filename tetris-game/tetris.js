254f54b31ee256344a31542d48000788c73aa0d2
NastyaVolk <nastysha19_97@mail.ru>
2017-08-29 21:36:16 +0300

    Tetris

--- tetris-game/tetris.js
+++ tetris-game/tetris.js
@@ -6,8 +6,23 @@ const context = canvas.getContext('2d');
 //Определяем масштаб (делаем его в 20 раз больше)
 context.scale(20,20);
 
-//Блок в форме буквы Т
 
+//Для пропадания строк
+function arenaSweep() {
+	let rowCount = 1;
+	outer: for (let y = arena.length - 1; y > 0; --y) {
+		for (let x = 0; x < arena[y].length; ++x) {
+			if (arena[y][x] === 0) {
+				continue outer;
+			}
+		}
+		const row = arena.splice(y, 1)[0].fill(0);
+		arena.unshift(row);
+		++y;
+		player.score += rowCount * 10;
+		rowCount *= 2;
+	}
+}
 
 //Проверяем на столкновения
 function collide(arena, player) {
@@ -31,54 +46,55 @@ while(h--) {
 return matrix;
 }
 
+//Разные блоки
 function createPiece(type) {
 	if (type === 'T' ) {
 	return [
-	[0,0,0],
-	[1,1,1],
-	[0,1,0],
+		[0,0,0],
+		[1,1,1],
+		[0,1,0],
 	];
 	}
 	else if (type === 'O') {
 		return [
 		[2, 2],
 		[2, 2],
-		];
+	];
 	}
 	else if (type === 'L') {
 		return [
-	[0,3,0],
-	[0,3,0],
-	[0,3,3],
+		[0,3,0],
+		[0,3,0],
+		[0,3,3],
 	];
 	}
 	else if (type === 'J') {
 	return [
-	[0,4,0],
-	[0,4,0],
-	[4,4,0],
+		[0,4,0],
+		[0,4,0],
+		[4,4,0],
 	];
 	}
 	else if (type === 'I') {
 	return [
-	[0,5,0, 0],
-	[0,5,0, 0],
-	[0,5,0, 0],
-	[0,5,0, 0],
+		[0,5,0, 0],
+		[0,5,0, 0],
+		[0,5,0, 0],
+		[0,5,0, 0],
 	];
 	}
 	else if (type === 'S') {
 	return [
-	[0,6,6],
-	[6,6,0],
-	[0,0,0],
+		[0,6,6],
+		[6,6,0],
+		[0,0,0],
 	];
 	}
 		else if (type === 'Z') {
 	return [
-	[7,7,0],
-	[0,7,7],
-	[0,0,0],
+		[7,7,0],
+		[0,7,7],
+		[0,0,0],
 	];
 	}
 }
@@ -124,6 +140,8 @@ function playerDrop() {
 		player.pos.y--;
 		merge(arena, player);
 		playerReset();
+		arenaSweep();
+		updateScore();
 		
 	}
 	dropCounter = 0;
@@ -136,6 +154,8 @@ function playerMove(dir) {
 		player.pos.x -= dir;
 	}
 }
+
+//Рандом для блоков
 function playerReset() {
 	const pieces = 'ILJOTSZ';
 	player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
@@ -144,9 +164,13 @@ function playerReset() {
 	(player.matrix[0].length / 2 | 0);
 	if (collide(arena, player)) {
 		arena.forEach(row => row.fill(0));
+		player.score = 0;
+		updateScore();
+
 	}
 }
 
+//Вращение блоков
 function playerRotate(dir) {
 	const pos = player.pos.x;
 	let offset = 1;
@@ -202,6 +226,12 @@ function update(time = 0) {
 	requestAnimationFrame(update);
 }
 
+function updateScore () {
+	document.getElementById('score').innerText = player.score;
+
+}
+
+//Цвета для блоков
 const colors = [
 null,
 'red',
@@ -215,10 +245,11 @@ null,
 
 const arena = createMatrix(12, 20);
 
-
+// Информация об игроке
 const player = {
-	pos: {x: 5, y: 5},
-	matrix: createPiece('T'),
+	pos: {x: 0, y: 0},
+	matrix: null,
+	score: 0,
 }
 
 //Обработка нажатия кнопок
@@ -231,7 +262,6 @@ document.addEventListener('keydown', event =>  {
 	//Двигаем вправо
 	else if (event.keyCode === 39) {
 		playerMove(1);
-		
 	}
 	//Двинаем вниз
 	else if (event.keyCode === 40) {
@@ -246,6 +276,8 @@ document.addEventListener('keydown', event =>  {
 
 });
 
+playerReset();
 update();
+updateScore();
 
 
